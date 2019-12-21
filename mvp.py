0f7daecccda71278d48f98ff997a5be3642c77cb
Mats Maiwald <mats.maiwald@gmail.com>
2019-12-22 00:48:34 +0100

    Add loop over years

--- mvp.py
+++ mvp.py
@@ -7,6 +7,23 @@ for col in ['snp500', '3mon', '10yr']:
     df[col] = df[col].str.rstrip('%').astype('float') / 100.0
 df = df.set_index('year')
 
+def get_asset_loadings():
+    question = "What percentage of your portfolio do you want to invest in?"
+    print("SnP500 loading")
+    loading_snp = input("Please enter a decimal fraction.")
+    print("3mon loading")
+    loading_3mon = input("Please enter a decimal fraction.")
+    print("10yr loading")
+    loading_10yr = input("Please enter a decimal fraction.")
+    loadings = np.array(
+        [
+        loading_snp,
+        loading_3mon,
+        loading_10yr
+        ]
+        )
+    return loadings
+
 class asset_markets:
     def __init__(self, df_asset_returns):
         self.df_asset_returns = df_asset_returns
@@ -21,15 +38,19 @@ class portfolio:
 
     def update_wealth(self, asset_returns):
         portfolio_return = (
-            np.matmul(self.loadings, asset_returns)
+            np.matmul(self.loadings, asset_returns.to_numpy())
             )
         self.wealth = self.wealth * (1 + portfolio_return)
 
 my_portfolio = portfolio(1000,  np.array([0.25, 0.25, 0.5]))
 my_asset_markets = asset_markets(df_asset_returns=df) 
 
-# total_wealth = 1000
-# loadings = np.array([0.25, 0.25, 0.5])
-returns_1928 = df.to_numpy()[3,:].T
-# net_return = np.matmul(loadings, df.to_numpy()[3,:].T)
-# total_wealth_new = total_wealth * (1 + net_return)
+for year in range(df.index.min(), df.index.max()):
+    print("It is the end of the year {}.".format(year))
+    current_returns = my_asset_markets.yield_returns(year=year)
+    my_portfolio.update_wealth(current_returns)
+    print("The portfolio has a value of {}."
+    .format(round(my_portfolio.wealth, 0)))
+    change_loadings = input("Would you like to change your asset loadings? (y/n)")
+    if change_loadings == "y":
+        my_portfolio.loadings = get_asset_loadings()
